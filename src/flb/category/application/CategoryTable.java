3986d44efe7546943666278f1830dfe25d3367e6
Jeff Tipps <jeff.tipps@ni.com>
2020-10-14 23:27:56 -0500

    Moved renderer into its own class

--- src/flb/category/application/CategoryTable.java
+++ src/flb/category/application/CategoryTable.java
@@ -3,9 +3,6 @@ package flb.category.application;
 import flb.category.Category;
 
 import javax.swing.*;
-import javax.swing.table.*;
-import java.awt.*;
-import java.text.DecimalFormat;
 import java.util.*;
 
 public class CategoryTable {
@@ -63,20 +60,5 @@ public class CategoryTable {
     public void rowChangeRefresh(ArrayList<Category> tableContents){
         tableModel.setContents(tableContents);
         tableModel.fireTableDataChanged();
-    }
-
-    static class GoalRenderer extends DefaultTableCellRenderer {
-        private static final DecimalFormat formatter = new DecimalFormat("#.00");
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            if (value.equals(Float.NaN)) {
-                value = "";
-            }
-            else{
-                value = formatter.format(value);
-            }
-            return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        }
     }
 }
